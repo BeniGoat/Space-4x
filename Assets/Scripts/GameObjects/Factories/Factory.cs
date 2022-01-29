fdf9858ab8d7ca972dcd7f9ad5d3b0d6d6a4080c
Ben Moloney <benigoat@gmail.com>
2022-01-29 14:00:40 +0000

    Commit assets

---
+++ Assets/Scripts/GameObjects/Factories/Factory.cs
@@ -0,0 +1,13 @@
+﻿namespace Unity.Space4x.Assets.Scripts.GameObjects.Factories
+{
+        /// <summary>
+        /// Factory design pattern with generic.
+        /// </summary>
+        public class Factory<T> : IFactory<T> where T : new()
+        {
+                public T Create()
+                {
+                        return new T();
+                }
+        }
+}
---
+++ Assets/Scripts/GameObjects/Sun.cs
@@ -0,0 +1,6 @@
+﻿namespace Unity.Space4x.Assets.Scripts.GameObjects
+{
+        public class Sun : SystemBody
+        {
+        }
+}
